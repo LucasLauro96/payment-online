449f3ad9a1e6ef2952cef1a0d01add47d46a61c2
Lucas Lauro <lucaskns1996@gmail.com>
2021-06-03 16:52:40 -0300

    first commit

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <title>Pagamentos Online</title>
+</head>
+
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <!-- JS -->
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+
+</html>
